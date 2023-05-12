93a322121857242453711081b7b690d6ac8c9bc3
shannaurelle <shannaurelleg@gmail.com>
2023-05-13 00:59:33 +0800

    feature: least frequently used algorithm template

---
+++ LFU.java
@@ -0,0 +1,28 @@
+public class LFU extends PageReplacementAlgorithm{
+
+    // this class implements the least frequently used algorithm
+    // input: pages to get from storage
+    // output: the hit / miss status
+    //         the 2-dimensional hit/miss matrix for each page requested
+    //         the 2-dimensional page frame matrix showing the page contents
+    //         other outputs inherited from the PageReplacementAlgorithm class 
+    boolean[][] hitMatrix;
+    int[][] framesMatrix;
+    // priority queue (reversed) needed to easily perform LFU
+
+    public LFU(int[] pages, int numOfPages, int frameSize){
+        super(frameSize);
+        framesMatrix = new int[numOfPages][frameSize];
+        hitMatrix = new boolean[numOfPages][frameSize];
+        // execute the algorithm
+    }
+
+    public boolean[][] getHitMatrix() {
+        return hitMatrix;
+    }
+
+    public int[][] getFramesMatrix() {
+        return framesMatrix;
+    }
+
+}
