e7b437cee1fc249fb31ab22de33ac78cbd8c4ed6
shannaurelle <shannaurelleg@gmail.com>
2023-05-13 00:57:30 +0800

    feature: ESC algorithm template

---
+++ ESC.java
@@ -0,0 +1,28 @@
+public class SC extends PageReplacementAlgorithm{
+
+    // this class implements the second chance algorithm
+    // input: pages to get from storage
+    // output: the hit / miss status
+    //         the 2-dimensional hit/miss matrix for each page requested
+    //         the 2-dimensional page frame matrix showing the page contents
+    //         other outputs inherited from the PageReplacementAlgorithm class 
+    boolean[][] hitMatrix;
+    int[][] framesMatrix;
+    // circular linked list needed to easily perform SC
+
+    public SC(int[] pages, int numOfPages, int frameSize){
+        super(frameSize);
+        framesMatrix = new int[numOfPages][frameSize];
+        hitMatrix = new boolean[numOfPages][frameSize];
+        // execute the algorithm
+    }
+
+    public boolean[][] getHitMatrix() {
+        return hitMatrix;
+    }
+
+    public int[][] getFramesMatrix() {
+        return framesMatrix;
+    }
+
+}
