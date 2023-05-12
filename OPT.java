38f59db208c3c8725dd81949c0c389a53126542c
shannaurelle <shannaurelleg@gmail.com>
2023-05-13 00:54:22 +0800

    feature: OPT algorithm template

---
+++ OPT.java
@@ -0,0 +1,28 @@
+public class OPT extends PageReplacementAlgorithm{
+
+    // this class implements the optimal replacement algorithm
+    // input: pages to get from storage
+    // output: the hit / miss status
+    //         the 2-dimensional hit/miss matrix for each page requested
+    //         the 2-dimensional page frame matrix showing the page contents
+    //         other outputs inherited from the PageReplacementAlgorithm class 
+    boolean[][] hitMatrix;
+    int[][] framesMatrix;
+    // doubly linked list needed to easily perform LRU
+
+    public OPT(int[] pages, int numOfPages, int frameSize){
+        super(frameSize);
+        framesMatrix = new int[numOfPages][frameSize];
+        hitMatrix = new boolean[numOfPages][frameSize];
+        // execute the algorithm
+    }
+
+    public boolean[][] getHitMatrix() {
+        return hitMatrix;
+    }
+
+    public int[][] getFramesMatrix() {
+        return framesMatrix;
+    }
+
+}
