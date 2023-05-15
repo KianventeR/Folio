e62b96f53de3d7184aa2059d38f8fbefce8bfd03
shannaurelle <shannaurelleg@gmail.com>
2023-05-15 15:07:06 +0800

    feature: initialize head as 0

--- src/SC.java
+++ src/SC.java
@@ -10,7 +10,7 @@ public class SC extends PageReplacementAlgorithm{
     boolean[][] hitMatrix;
     int[][] framesMatrix;
     // circular linked list needed to easily perform SC
-    int head; // moving head to mimic a circular queue
+    int head = 0; // moving head to mimic a circular queue
     int[] refBits; // reference bits for second chance
 
     public SC(int[] pages, int numOfPages, int frameSize){
@@ -18,6 +18,22 @@ public class SC extends PageReplacementAlgorithm{
         framesMatrix = new int[numOfPages][frameSize];
         hitMatrix = new boolean[numOfPages][frameSize];
         // execute the algorithm
+        for(int iter = 0; iter < numOfPages; iter++){
+            // if page is found in the pageFrames array don't insert
+            if(foundPage(pages[iter])){
+                hits[iter] = true;
+            }else{
+                // else insert it to topmost frame
+                pageFrames[pageCount] = pages[iter];
+                pageCount++;
+                hits[iter] = false;
+            }
+            // then save it to the matrix for the iteration
+            for(int i = 0; i < frameSize; i++){
+                framesMatrix[iter][i] = pageFrames[i];
+                hitMatrix[iter][i] = hits[i];
+            }
+        }
     }
 
     public boolean[][] getHitMatrix() {
