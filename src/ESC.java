3979a17930bfe34d376eb8f52f9226a97de47768
shannaurelle <shannaurelleg@gmail.com>
2023-05-18 11:37:05 +0800

    refactor: add initial second chance algo first

--- src/ESC.java
+++ src/ESC.java
@@ -10,15 +10,58 @@ public class ESC extends PageReplacementAlgorithm{
     //         other outputs inherited from the PageReplacementAlgorithm class 
     boolean[][] hitMatrix;
     int[][] framesMatrix;
-    int[] modifyBits; 
-    int[] refBits; // reference bits for second chance
+    
+    
     // circular linked list needed to easily perform ESC
+    int head; // pointer for start of circular linked list
+    int[] refBits; // reference bits for second chance
+    int[] modifyBits; // modify bits for enhanced second chance
 
     public ESC(int[] pages, int numOfPages, int frameSize){
         super(frameSize);
         framesMatrix = new int[numOfPages][frameSize];
         hitMatrix = new boolean[numOfPages][frameSize];
         // execute the algorithm
+        for(int iter = 0; iter < numOfPages; iter++){
+            // if page is found in the pageFrames array don't insert
+            if(foundPage(pages[iter])){
+                hits[iter] = true;
+                // find where to give second chance
+                for(int i = 0; i < pageCount; i++){
+                    if(pages[iter] == pageFrames[i]){
+                        refBits[i] = 1; // second chance creation
+                        break;
+                    }
+                }
+            }else{
+                // search for the page to be replaced
+                // start at head
+                int index = head;
+                for(int i = 0; i < pageCount; i++){
+                    // remove first page with refBit = 0
+                    int frame = (index+i) % frameSize;
+                    if(refBits[frame] == 0){
+                        pageFrames[frame] = 0;
+                        refBits[frame] = 0;
+                    }
+                    // use the second chance for the rest of pages
+                    if(refBits[i] == 1){
+                        refBits[i] = 0;
+                    }
+                }
+                // else insert it to the chosen page frame
+                pageFrames[index] = pages[iter];
+                pageCount++;
+                hits[iter] = false;
+            }
+            // move the circular queue head. page hit or page fault
+            head = (head + 1) % frameSize;
+            // then save it to the matrix for the iteration
+            for(int i = 0; i < frameSize; i++){
+                framesMatrix[iter][i] = pageFrames[i];
+                hitMatrix[iter][i] = hits[i];
+            }
+        }
     }
 
     public boolean[][] getHitMatrix() {
